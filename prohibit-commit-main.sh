2aa33c8498efc5a452499c57d13309c240285b2f
jonasbn <jonasbn@gmail.com>
2021-06-13 19:22:29 +0200

    Addressed  issues reported by spellcheck

--- prohibit-commit-main.sh
+++ prohibit-commit-main.sh
@@ -2,7 +2,7 @@
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
-if [ "$branch" = "main" -o "$branch" = "master" ]; then
-  echo "Committing directly to main/master branch is prohibited, use -n if you really insist"
-  exit 1
+if [ "$branch" = "main" ] || [ "$branch" = "master" ]; then
+    echo "Committing directly to main/master branch is prohibited, use -n if you really insist"
+    exit 1
 fi
